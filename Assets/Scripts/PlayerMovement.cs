0dcee6792300b3de4fce3c05867c00b4b7c9451c
Jerome MR <10962717+Assasquid@users.noreply.github.com>
2021-11-17 16:43:19 +0000

    Adding ladders and the ability to climb them

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,12 +6,20 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 2f;
     Vector2 moveInput;
     Rigidbody2D playerRb;
+    Animator playerAnimator; 
+    CapsuleCollider2D playerCollider;
+    float normalPlayerGravityScale;
 
     void Awake()
     {
         playerRb = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+        playerCollider = GetComponent<CapsuleCollider2D>();
+        normalPlayerGravityScale = playerRb.gravityScale;
     }
 
     void Start()
@@ -23,6 +31,7 @@ public class PlayerMovement : MonoBehaviour
     {
         Run();
         FlipSprite();
+        ClimbLadder();
     }
 
     void OnMove(InputValue value)
@@ -30,10 +39,26 @@ public class PlayerMovement : MonoBehaviour
         moveInput = value.Get<Vector2>();
     }
 
+    void OnJump(InputValue value)
+    {
+        if(!playerCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+        
+        if(value.isPressed)
+        {
+            playerRb.velocity += new Vector2(0f, jumpSpeed);
+        }
+    }
+
     void Run()
     {
         Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, playerRb.velocity.y);
         playerRb.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(playerRb.velocity.x) > Mathf.Epsilon;
+        playerAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
     }
 
     void FlipSprite()
@@ -44,5 +69,22 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.localScale = new Vector2(Mathf.Sign(playerRb.velocity.x), 1f);
         }
+    }
+
+    void ClimbLadder()
+    {
+        if(!playerCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {
+            playerRb.gravityScale = normalPlayerGravityScale;
+            playerAnimator.SetBool("isClimbing", false);
+            return;
+        }
+
+        playerRb.gravityScale = 0;
+        Vector2 climbVelocity = new Vector2(playerRb.velocity.x, moveInput.y * climbSpeed);
+        playerRb.velocity = climbVelocity;
+
+        bool playerHasVerticalSpeed = Mathf.Abs(playerRb.velocity.y) > Mathf.Epsilon;
+        playerAnimator.SetBool("isClimbing", playerHasVerticalSpeed);
     }
 }
