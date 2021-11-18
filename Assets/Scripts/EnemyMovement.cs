c3e1f7ddfc328a16e814b3fa1d2ada90f70b7062
Jerome MR <10962717+Assasquid@users.noreply.github.com>
2021-11-18 20:06:57 +0000

    Set up slime enemy movement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    Rigidbody2D enemyRb;
+    
+    void Awake()
+    {
+        enemyRb = GetComponent<Rigidbody2D>();
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        enemyRb.velocity = new Vector2 (moveSpeed, 0f);
+    }
+
+    void OnTriggerExit2D(Collider2D other) 
+    {
+        moveSpeed = -moveSpeed;
+        FlipEnemyFacing();
+    }
+
+    void FlipEnemyFacing()
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(enemyRb.velocity.x)), 1f);
+    }
+}
